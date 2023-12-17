ea7aa49fb5175cbbc900135e8c69a07cf5bae87c
Inês Lidório <a70563@ualg.pt>
2023-12-17 18:09:09 +0000

    ALterações

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,37 +67,22 @@ const config = {
       navbar: {
         title: 'OFICINA',
         logo: {
-          alt: 'Logo',
+          alt: 'My Site Logo',
           src: 'img/logo.svg',
         },
-        //items: [
-          //{
-           // type: 'docSidebar',
-            //sidebarId: 'tutorialSidebar',
-            //position: 'left',
-            //label: 'Tutorial',
-          //},
+        items: [
           //{to: '/blog', label: 'Blog', position: 'left'},
-          //{
-           // href: 'https://github.com/facebook/docusaurus',
-           //label: 'GitHub',
-           //position: 'right',
-          //},
-        //],
-      //},
-      //footer: {
-        //style: 'dark',
-        //links: [
-          //{
-            //title: 'Docs',
-            //items: [
-              //{
-                //label: 'Tutorial',
-                //to: '/docs/intro',
-              //},
-           // ],
-          //},
-          //{
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+              },
+      footer: {
+        style: 'dark',
+        links: [
+          {
             title: 'Community',
             items: [
               {
