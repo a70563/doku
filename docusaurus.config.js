f7a9af0dced8caffc6726dfefbbe8773a094c309
Inês Lidório <a70563@ualg.pt>
2023-12-17 17:39:41 +0000

    Pequenas alterações

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Test Site',
-  tagline: 'Disciplina de ADC',
+  title: 'OFICINA WINRAR',
+  tagline: 'Sempre ao seu Dispor',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
