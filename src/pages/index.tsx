fc3837f6bbb8788f34f158e9bbd1c97a4534a8f5
Inês Lidório <a70563@ualg.pt>
2023-12-18 18:59:47 +0000

    s

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,3 +26,17 @@ function HomepageHeader() {
   );
 }
 
+export default 
+function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`ola ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
